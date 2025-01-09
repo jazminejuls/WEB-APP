6c28d6d92ae09f79b34f9c1b9ed4813a9b911214
jazminejuls <jazminejuliagalsim@gmail.com>
2025-01-09 23:36:19 +0800

    added new products and pages

---
+++ Flower Shop/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About us</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>Extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +63 905 377 7010 </p>
+            <p> <i class="fas fa-phone"></i> +63 994 885 1573 </p>
+            <p> <i class="fas fa-envelope"></i> julia@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> TUP - Manila </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; Copyright @ <?php echo date('Y'); ?> by <span>FloraLink</span> </div>
+
+</section>
